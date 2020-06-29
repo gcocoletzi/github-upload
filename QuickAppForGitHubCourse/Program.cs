2abce0dde4839dcdb08ef0a8036ad9df9fe69f06
Gilberto Cocoletzi <gcocoletzic@gmail.com>
2020-06-29 11:03:24 -0500

    initializing repository

---
+++ QuickAppForGitHubCourse/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace QuickAppForGitHubCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, type your name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello {0} !", name);            
+        }
+    }
+}
